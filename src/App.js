dda3fe2e2f4b2f5bf72010f4063da7f19ab0f4f9
Alison Feldish <alison.feldish@gmail.com>
2023-01-19 18:40:32 -0500

    syntax cleanup

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Game from "./Game";
 import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Game />
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+        <div className='App'>
+            <Game />
+        </div>
+        );
+    };
+};
 
 export default App;
